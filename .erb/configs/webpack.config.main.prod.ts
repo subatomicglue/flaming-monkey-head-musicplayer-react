6b8ce6cdc9eee1c5b67506ed53ae8d76d022f047
km <kmeinert@aviahealthinnovation.com>
2022-12-13 16:50:11 -0600

    added some comments, no functional changes

--- .erb/configs/webpack.config.main.prod.ts
+++ .erb/configs/webpack.config.main.prod.ts
@@ -24,6 +24,7 @@ const configuration: webpack.Configuration = {
 
   entry: {
     main: path.join(webpackPaths.srcMainPath, 'main.ts'),
+    // KEVIN: https://github.com/electron-react-boilerplate/electron-react-boilerplate/issues/2949
     preload: path.join(webpackPaths.srcMainPath, 'preload.ts'),
   },
 
